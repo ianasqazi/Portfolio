c21c608f1ddf07cd9d30a4fb106cba114dda354c
Anas Qazi <ianasqazi@gmail.com>
2019-11-26 02:40:18 -0500

    added moment js library and Open weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,53 @@ $(window).scroll(function(e) {
     }
 
 });
+
+$(document).ready(function(){
+
+    callWeatherAPI();
+});
+
+function callWeatherAPI(){
+    // var currentCity = $.trim($("#searchCity").val());
+    var currentCity = "Toronto";
+
+    var currentDate = moment().format("MMMM Do YYYY");
+    var currentDay = moment().format("dddd");
+    
+    var APIKey = "b72c0d35aba9f0b8c0e9ebb9ec68c3f8";
+  
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=metric&appid=" + APIKey;
+    
+    // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Toronto&units=metric&appid=" + APIKey;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+      }).then(function(response) {
+
+            $("#dayDate").text(currentDay+ ", " + currentDate);
+
+            $("#currentCity").text(response.name);
+            $("#currentCountry").text(", " + response.sys.country)
+            
+            $("#weatherInfo").text(response.weather[0].main);
+            var iconCode=response.weather[0].icon;
+            var iconURL="https://openweathermap.org/img/w/"+iconCode+".png";
+            $("#weatherIcon").attr("src",iconURL);
+            
+  
+            $("#currentTemp").text(":" + response.main.temp + String.fromCharCode(176) + "C");
+            $("#tempLow").text(":" + response.main.temp_min + String.fromCharCode(176) + "C");
+            $("#tempHigh").text(":" + response.main.temp_max + String.fromCharCode(176) + "C");
+
+            $("#humidity").text(":" + response.main.humidity + "%");
+            $("#pressure").text(":" + response.main.pressure + "hpa")
+            $("#windSpeed").text(":" + response.wind.speed + "m/s");
+  
+ 
+          });
+  
+          // Save the value of the searched element in the localStorage
+          localStorage.setItem("lastSearchedCity",currentCity);
+  
+    }
