c0c4ff3212f2be7d077479eda7d9e13a7dc4b331
Anas Qazi <ianasqazi@gmail.com>
2020-06-01 14:22:11 -0400

    added experience

---
+++ me.js
@@ -0,0 +1,22 @@
+let Me = {
+    name: 'Anas Qazi',
+    title: 'Full Stack Web Developer',
+    techSkills : {
+        frontEnd:['React.js', 'Angular.js', 'Vue.js', '', '', '', '', '', ''],
+        backEnd:[, '', '', '', '', '', '', '', '', '', ''],
+        frameWorks: [, '', '', '', '', '', '', '', '', '']
+    }
+
+}
+
+console.log(`I am {{Me.name}}`)
+document.getElementById('message').innerHTML=
+(`I am $()`)
+
+Languages: JavaScript, HTML, CSS
+Frameworks / Libraries: React, Express, Node, jQuery, Bootstrap, Materialize, AJAX, Axios, Handlebars
+Databases / ORM: Oracle, Mongo DB, MySQL, Sequelize, Mongoose, Jaws DB
+Collaboration / Deployment: Git, GitHub, Heroku, Travis
+Operating Systems: Linux, Unix, HP-UX, Solaris, Mac OS, Windows
+Oracle Expertise: Oracle databases, Real Application Cluster, Data Guard, E-Business Suite, Enterprise Manager, ASM, SAN, RMAN.
+Amazon Web Services: S3, EC2, Fargate, ECS, ECR, ELB, VPC, AIM, RDS, DynamoDB, CodeCommit, CodePipeline, CodeDeploy, CodeBuild
